cede4f0887b54ddcd4a98727cc4a3696926d21fe
Hilmi AM <imiia75775@proton.me>
2024-01-27 13:29:05 +0700

    Initial commit

---
+++ src/main/java/franxx/code/sequencedcollection/JavaSequencedCollectionApplication.java
@@ -0,0 +1,13 @@
+package franxx.code.sequencedcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSequencedCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSequencedCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/franxx/code/sequencedcollection/JavaSequencedCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package franxx.code.sequencedcollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSequencedCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
